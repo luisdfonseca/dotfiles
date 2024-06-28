cce53af475064e4cde33d87d5f38a162e77e332c
Luis Daniel Fonseca <luisdaniel309@gmail.com>
2024-06-28 16:58:31 -0400

    Update init.lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -315,9 +315,9 @@ vim.o.termguicolors = true
 --
 -- LDF
 
-vim.api.nvim_set_keymap('n', '<leader>k', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>t', ':tabnew<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>n', ':tabnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>b', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>n', ':tabnew<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><Tab>', ':tabnext<CR>', { noremap = true, silent = true })
 
 
 -- LDF END
